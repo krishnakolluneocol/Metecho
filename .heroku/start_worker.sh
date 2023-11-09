54347709c89b8e0e5f5b6ab2d5b19c6f6f3b2661
Krishna Kollu <93388033+krishnakolluneocol@users.noreply.github.com>
2023-11-09 07:38:18 -0500

    Comment out mkdir

--- .heroku/start_worker.sh
+++ .heroku/start_worker.sh
@@ -2,6 +2,8 @@ set -e
 
 # make Chrome available to VBT
 # Copied from https://github.com/SFDO-Tooling/MetaDeploy/pull/3468
-mkdir -p /opt/google/chrome
-ln -s /app/.apt/usr/bin/google-chrome /opt/google/chrome/chrome
+
+# todo - uncomment this after issue is resolved
+#mkdir -p /opt/google/chrome
+#ln -s /app/.apt/usr/bin/google-chrome /opt/google/chrome/chrome
 python manage.py rqworker default
