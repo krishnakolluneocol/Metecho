3bd7fac0cbbdffef5aa1482d3c4c3733af199d86
Krishna Kollu <93388033+krishnakolluneocol@users.noreply.github.com>
2023-11-14 15:49:57 -0500

    Add smoke test

--- .heroku/chrome-smoke-test.py
+++ .heroku/chrome-smoke-test.py
@@ -3,6 +3,10 @@ import selenium.webdriver
 import sys
 soptions = selenium.webdriver.chrome.options.Options()
 soptions.headless = True
+soptions.add_argument("--no-sandbox")
+soptions.add_argument("--headless")
+soptions.add_argument("--disable-dev-shm-usage")
+soptions.add_argument('--disable-gpu')
 try:
     driver = selenium.webdriver.Chrome(options=soptions)
     driver.quit()
