08c9bdd00501cb728ca4f68ef1d7c5fd7aaf23d5
Krishna Kollu <93388033+krishnakolluneocol@users.noreply.github.com>
2023-11-14 08:17:31 -0500

    add chrome smoke test

---
+++ .heroku/chrome-smoke-test.py
@@ -0,0 +1,11 @@
+import selenium
+import selenium.webdriver
+import sys
+options = selenium.webdriver.chrome.options.Options()
+options.headless = True
+try:
+    selenium.webdriver.Chrome(options=options)
+    print(f"Able to open chrome")
+except Exception as e:
+    print(f"Unable to open chrome:\n{e}")
+    sys.exit(1)
