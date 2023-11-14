c8204300d5d138f62cce565c652989d040dd2302
Krishna Kollu <93388033+krishnakolluneocol@users.noreply.github.com>
2023-11-14 17:37:32 -0500

    Add binary_location

--- .heroku/chrome-smoke-test.py
+++ .heroku/chrome-smoke-test.py
@@ -3,11 +3,7 @@ import selenium.webdriver
 import sys
 soptions = selenium.webdriver.chrome.options.Options()
 soptions.headless = True
-soptions.add_argument("--no-sandbox")
-soptions.add_argument("--headless")
-soptions.add_argument("--disable-dev-shm-usage")
-soptions.add_argument('--disable-gpu')
-soptions.add_argument('--remote-debugging-port=9222')
+soptions.binary_location = '/app/.apt/usr/bin/google-chrome'
 try:
     driver = selenium.webdriver.Chrome(options=soptions)
     driver.quit()
