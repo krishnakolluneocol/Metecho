04cd30a478e071948ed4f142d0dfac5aaa57d46a
Krishna Kollu <93388033+krishnakolluneocol@users.noreply.github.com>
2023-11-15 10:43:15 -0500

    Revert selenium version

--- .heroku/chrome-smoke-test.py
+++ .heroku/chrome-smoke-test.py
@@ -3,7 +3,6 @@ import selenium.webdriver
 import sys
 soptions = selenium.webdriver.chrome.options.Options()
 soptions.headless = True
-soptions.binary_location = '/app/.apt/usr/bin/google-chrome' 
 try:
     driver = selenium.webdriver.Chrome(options=soptions)
     driver.quit()
