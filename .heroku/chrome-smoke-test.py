7a302303a26bc708ab55e8b2ef80c01210d976b5
Krishna Kollu <93388033+krishnakolluneocol@users.noreply.github.com>
2024-01-08 09:11:52 -0500

    Uodate

--- .heroku/chrome-smoke-test.py
+++ .heroku/chrome-smoke-test.py
@@ -6,7 +6,7 @@ soptions.headless = True
 try:
     driver = selenium.webdriver.Chrome(options=soptions)
     driver.quit()
-    print(f"Able to open chrome") 
+    print(f"Able to open chrome.") 
 except Exception as e:
     print(f"Unable to open chrome:\n{e}")
     sys.exit(1)  
